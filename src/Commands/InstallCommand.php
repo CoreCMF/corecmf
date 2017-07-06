76d05f0e0eb19e61f1904926b414a2020a195cab
bigrocs <bigrocs@qq.com>
2017-07-06 14:34:24 +0800

    增加安装卸载

---
+++ src/Commands/InstallCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace CoreCMF\corecmf\Commands;
+
+use Illuminate\Console\Command;
+
+use CoreCMF\core\Support\Commands\Install;
+
+class InstallCommand extends Command
+{
+    /**
+     *  install class.
+     * @var object
+     */
+    protected $install;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     * @translator laravelacademy.org
+     */
+    protected $signature = 'corecmf:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'corecmf install';
+
+    public function __construct(Install $install)
+    {
+        parent::__construct();
+        $this->install = $install;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info($this->install->publish('public'));
+    }
+}
---
+++ src/Commands/UninstallCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CoreCMF\admin\Commands;
+
+use Illuminate\Console\Command;
+
+use CoreCMF\core\Support\Commands\Uninstall;
+class UninstallCommand extends Command
+{
+    protected $uninstall;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     * @translator laravelacademy.org
+     */
+    protected $signature = 'corecmf:uninstall';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'corecmf uninstall';
+
+    public function __construct(Uninstall $uninstall)
+    {
+        parent::__construct();
+        $this->uninstall = $uninstall;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+    }
+}
