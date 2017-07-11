996a01084f3caa2a7d19d9e770848a41ce0ee1f9
bigrocs <bigrocs@qq.com>
2017-07-11 14:51:35 +0800

    完成steps0

--- src/Controllers/Api/InstallController.php
+++ src/Controllers/Api/InstallController.php
@@ -4,72 +4,95 @@ namespace CoreCMF\corecmf\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use CoreCMF\core\Support\Http\Request as CoreRequest;
 
 class InstallController extends Controller
 {
-    public function index(Request $Request)
+    private $builderForm;
+
+    public function __construct()
     {
-      $steps = 0;
+        $this->builderForm = resolve('builderForm');
+    }
+    public function index(CoreRequest $request)
+    {
+      $steps        = $request->get('steps',0);
       $stepsTitle = ['用户协议','检测环境','数据库设置','账户设置','安装完成'];
-      $form = resolve('builderForm')
-              ->item(['name' => 'steps',      'type' => 'steps',   'title'=>$stepsTitle, 'active'=>$steps])
+      $this->builderForm->item(['name' => 'steps',      'type' => 'steps',   'title'=>$stepsTitle, 'active'=>$steps])
               ->apiUrl('submit',route('api.install.index'))
               ->config('formStyle',['padding'=>'20px 10px' ])
-              ->config('labelWidth','0');
+              ->config('labelWidth','0')
+              ->config('formReset',['style'=> ['display'=>'none'] ]);
       switch ($steps) {
         case 0:
-          $form = $this->steps0($form);
+          $this->steps0();
           break;
         case 1:
-          $form = $this->steps1($form);
+          $this->steps1();
           break;
         case 2:
-          $form = $this->steps2($form);
+          $this->steps2();
           break;
         case 3:
-          $form = $this->steps3($form);
+          $this->steps3();
           break;
         case 4:
-          $form = $this->steps4($form);
+          $this->steps4();
           break;
         case 5:
-          $form = $this->steps5($form);
+          $this->steps5();
           break;
       }
       $html = resolve('builderHtml')
                 ->title('系统安装')
-                ->item($form)
+                ->item($this->builderForm)
+                ->config('refresh',false)
                 ->response();
       return $html;
     }
-    public function steps0($form){
-        $form->item(['name' => 'agreement',  'type' => 'scrollbar', 'value' => config('corecmf.agreement'),])
-             ->item(['name' => 'password',   'type' => 'password',    'placeholder' => '0']);
-        return $form;
+    public function steps0(){
+        $this->builderForm->item([
+                'name' => 'agreement',
+                'type' => 'scrollbar',
+                'value' => config('corecmf.agreement'),]
+             )
+             ->item([
+               'name' => 'userAgreement',
+               'type' => 'checkbox',
+               'value'=>[],
+               'options' => [
+                 'userAgreement' => [
+                   'name'=>'我同意相关用户协议',
+                   'eventEnable'=>['submitButton']
+                 ]
+               ]
+             ])
+             ->item(['name' => 'steps','type' => 'hidden','value' => 1,])
+             ->config('formSubmit',[ 'name'=>'安装', 'disabled'=> true, 'style'=> ['width'=>'38.2%'] ]);
     }
-    public function steps1($form){
-        $form->item(['name' => 'agreement',  'type' => 'scrollbar', 'value' => config('corecmf.agreement'),])
+    public function steps1(){
+        $this->builderForm
+             ->item(['name' => 'agreement',  'type' => 'scrollbar', 'value' => config('corecmf.agreement'),])
              ->item(['name' => 'password',   'type' => 'password',    'placeholder' => '1']);
-        return $form;
     }
-    public function steps2($form){
-        $form->item(['name' => 'agreement',  'type' => 'scrollbar', 'value' => config('corecmf.agreement'),])
+    public function steps2(){
+        $this->builderForm
+             ->item(['name' => 'agreement',  'type' => 'scrollbar', 'value' => config('corecmf.agreement'),])
              ->item(['name' => 'password',   'type' => 'password',    'placeholder' => '2']);
-        return $form;
     }
-    public function steps3($form){
-        $form->item(['name' => 'agreement',  'type' => 'scrollbar', 'value' => config('corecmf.agreement'),])
+    public function steps3(){
+        $this->builderForm
+             ->item(['name' => 'agreement',  'type' => 'scrollbar', 'value' => config('corecmf.agreement'),])
              ->item(['name' => 'password',   'type' => 'password',    'placeholder' => '3']);
-        return $form;
     }
-    public function steps4($form){
-        $form->item(['name' => 'agreement',  'type' => 'scrollbar', 'value' => config('corecmf.agreement'),])
+    public function steps4(){
+        $this->builderForm
+             ->item(['name' => 'agreement',  'type' => 'scrollbar', 'value' => config('corecmf.agreement'),])
              ->item(['name' => 'password',   'type' => 'password',    'placeholder' => '4']);
-        return $form;
     }
-    public function steps5($form){
-        $form->item(['name' => 'agreement',  'type' => 'scrollbar', 'value' => config('corecmf.agreement'),])
+    public function steps5(){
+        $this->builderForm
+             ->item(['name' => 'agreement',  'type' => 'scrollbar', 'value' => config('corecmf.agreement'),])
              ->item(['name' => 'password',   'type' => 'password',    'placeholder' => '5']);
-        return $form;
     }
 }
