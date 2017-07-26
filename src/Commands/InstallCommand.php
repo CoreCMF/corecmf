b276790caabf2828abe19ef61514e16179ab9891
bigrocs <bigrocs@qq.com>
2017-07-26 17:22:44 +0800

    关联用户权限组

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -85,6 +85,7 @@ class InstallCommand extends Command
             'integral'  => 0,
             'money'     => 0,
         ]);//插入关联数据库userInfos
+        $user->roles()->attach($user->id);//多对多关联 关联用户id
         touch(storage_path() . DIRECTORY_SEPARATOR . 'installed');//锁定安装文件
         return true;
     }
